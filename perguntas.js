23407c4bbc64dffbc3baa50d807a87b9060fd3db
capitaomarvel <claraoliveiradasilva53@gmail.com>
2024-11-26 23:40:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,83 @@
+criaCartao(
+    "Portugues",
+    "Quais são as eras do romantismo no Brasil?",
+    "Primeira geração: nacionalista, indianista e religiosa Segunda geraçãogeração do mal-do-século ou Ultrarromântica Terceira geração: geração condoreira"
+)
+
+criaCartao(
+    "Tecnologia",
+    "O que é GiHub",
+    "A plataforma com um banco de armazenamento com códigos de programação, além de auxiliar os programadores em seus projetos, também é uma rede de compartilhamento e troca de conhecimento entre os profissionais dessa área."
+)
+
+criaCartao(
+    "Biotecnologia",
+    "Como a técnica de CRISPR-Cas9 tem sido utilizada no desenvolvimento de terapias genéticas para doenças hereditárias?,
+    "A técnica de **CRISPR-Cas9** é usada para corrigir mutações causadoras de doenças hereditárias ao editar genes diretamente no DNA. Primeiro, identifica-se o gene defeituoso. Depois, um RNA guia direciona a proteína Cas9 ao local específico no genoma, onde ela faz um corte. Esse corte é reparado pela célula, permitindo a substituição ou correção do gene defeituoso. Aplicações incluem o tratamento de condições como anemia falciforme, distrofia muscular e fibrose cística. A técnica oferece precisão e personalização, embora ainda enfrente desafios como a garantia de segurança e eficácia."
+)
+
+criaCartao(
+    "Matematica",
+    "O que é log ?",
+    "O logaritmo é o inverso da operação de exponenciação Ex: Qual é o expoente necessário para elevar uma base a fim de obter um determinado númro"
+)
+
+criaCartao(
+    "Matematica",
+    "O que é fração ?",
+    "A fração é uma forma de representar uma parte de um todo ou uma razão entre dois números"
+)
+
+criaCartao(
+    "Matematica",
+    "O que é fórmula de bhaskara ?",
+    "é um cálculo matemático que permite resolver equações do segundo grau, também conhecidas como equações quadráticas"
+)
+
+criaCartao(
+    "Historia",
+    "Por que a ditadura aconteceu? E quando aconteceu?",
+    "A Ditadura Militar no Brasil iniciou-se em 1964, por meio de um golpe civil-militar contra o presidente João Goulart e estendeu-se até 1985, sendo marcada pela repressão."
+)
+
+criaCartao(
+    "Historia",
+    " Qual foi o golpe de collor?",
+    "Em 16 de março de 1990, o ex-presidente implementou o Plano Collor, que consistia no confisco das cadernetas de poupança por 18 meses. Estima-se que o governo tenha confiscado cerca de US$ 100 bilhões, o equivalente a 30% do Produto Interno Bruto (PIB) do Brasil naquele ano"
+)
+
+criaCartao(
+    "Sociologia",
+    "O que é capitalismo ?",
+    "é um sistema econômico que se baseia na propriedade privada dos meios de produção, na acumulação de capital e no lucro."
+)
+
+criaCartao(
+    "Geografia",
+    " O que é gentrificação?",
+    " processo de transformação socioeconômica e cultural de um bairro ou região, que leva à expulsão de moradores antigos e à valorização mercadológica do local"
+)
+
+criaCartao(
+    "Geografia",
+    "O que é segregação socioespacial?",
+    "Segregação socioespacial é a divisão de classes sociais em áreas específicas de uma cidade"
+)
+
+criaCartao(
+    "Biologia",
+    "O que é o simbolo triângular nas embalagens de alimentos?",
+    "indicativo da presença de componentes geneticamente modificados - os chamados transgênicos"
+)
+
+criaCartao(
+    "Biologia",
+    "Por que duplicamos o DNA humano?",
+    "A replicação do DNA ou duplicação do DNA é um processo de grande importância para a transmissão do material genético"
+)
+
+criaCartao(
+    "Biologia",
+    " O que IST?",
+    "Infecções Sexualmente Transmissíveis"
+)
